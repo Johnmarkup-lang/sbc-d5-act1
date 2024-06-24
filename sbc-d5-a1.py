b4976daf004666cbc31d0e1c1c7842bb33b5450a
JOHNMARK <hoomanthe3rd@gmail.com>
2024-06-23 23:15:57 -0700

    Add files via upload

---
+++ sbc-d5-a1.py
@@ -0,0 +1,12 @@
+l = []
+l.append(int(input("butang number: "))); l.append(int(input("butang number: "))); l.append(int(input("butang number: "))); l.append(int(input("butang number: ")))
+#e.g: 1 2 3 4
+#     0 1 2 3
+print ("DISPLAY INPUTTED NUMBERS: ", l)
+head = input("naa si boss? (oo/wala): ")
+
+if head == 'oo':
+    l.pop(0)
+else:
+    l.pop()
+print(l)
